4ef5d328d1999384643bf184d2c98ad37e11ac3d
Mikołaj Wawrzyniak <mikolajspejs@gmail.com>
2017-10-29 21:10:19 +0100

    Add formatted output

--- CombatXP.lua
+++ CombatXP.lua
@@ -17,15 +17,19 @@ function CombatXP:Initialize()
 end
 
 function CombatXP.onPlayerExperienceGained(event, unitTag, currentExp, maxExp, reason)
-    
+
     if ( unitTag ~= 'player' ) then return end
        local xp_gained = currentExp - CombatXP.currentXP
-       CHAT_SYSTEM:AddMessage(tostring(xp_gained) .. " experience gained!")
+       local percent_to_next_level = (currentExp / maxExp) * 100
+       local percent_to_next_as_string = string.sub(tostring(percent_to_next_level),0,2) .. "%"
+       local current_gain_in_percent = (xp_gained / maxExp) * 100
+       local current_gain_in_percent_as_string = string.sub(tostring(current_gain_in_percent),0,4) .. "%"
+       CHAT_SYSTEM:AddMessage(tostring(xp_gained) .. " experience gained! " .. percent_to_next_as_string)
        CombatXP.currentXP = currentExp
 
     if CombatXP.inCombat then 
         CombatXP.gained_in_current_combat = CombatXP.gained_in_current_combat + xp_gained
-        CombatXPIndicatorLabel:SetText(tostring("XP gained in last combat: +" .. tostring(CombatXP.gained_in_current_combat)))
+        CombatXPIndicatorLabel:SetText(tostring("XP gained in last combat: +" .. tostring(CombatXP.gained_in_current_combat)  .. " (" .. current_gain_in_percent_as_string .. ")" .. " \n " .. currentExp .. "/" .. maxExp .. " (" .. percent_to_next_as_string .. ")"))
         CombatXPIndicator:SetHidden(false)
     else
         CombatXP.gained_in_current_combat = 0
